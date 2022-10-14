fc824bafad2375c502ef04169c7cfabcfd1651fc
Pedro R <rochapedro436@gmail.com>
2022-10-14 15:30:30 -0300

    first commit

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className='footer'>
+        <span>
+            Desenvolvido com <i className='fa fa-heart text-danger'></i>por 
+            <strong>
+                <span className='text-danger'>Pedro</span>
+            </strong>
+        </span>
+    </footer>
